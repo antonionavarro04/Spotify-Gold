8b22792be1f58e08de5b9878fa24085548158a05
Navarro <antnavarro@iesnervion.es>
2023-11-27 08:16:12 +0100

    Backup - 08:16 | 2023-11-27

--- Spotify Gold API/pytubeExample.py
+++ Spotify Gold API/pytubeExample.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 
 # Download only audio
-url = 'https://www.youtube.com/watch?v=ZDuoUAAU0ug'
+url = 'https://www.youtube.com/watch?v=QU1pPzEGrqw'
 yt = YouTube(url)
 vid = yt.streams.filter(only_audio=True).first()
 vid.download()
