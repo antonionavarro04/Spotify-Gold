0e376da5d945899a7022f4d395c9427a71259c71
antonionavarro04 <antonio.navarro@iesnervion.es>
2024-04-02 20:27:07 +0200

    .gitignore Remake

--- SpotifyGoldServer/Controllers/APIController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace SpotifyGoldServer.Controllers {
-    public class APIController : Controller {
-        public IActionResult Index() {
-            return RedirectToAction("Index", "Home");
-        }
-    }
-}
--- SpotifyGoldServer/Models/ServerFunctionsManager.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SpotifyGoldServer.Models {
-	internal class ServerFunctionsManager {
-
-		public static bool DeleteDirectoriesAfterADownload { get; } = true;
-
-	}
-}
