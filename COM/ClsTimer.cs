67d8db6263a38c318812d645506ce757697fd61d
antonionavarro04 <antonio.navarro@iesnervion.es>
2024-04-17 14:03:32 +0200

    ClsTimmer

---
+++ COM/ClsTimer.cs
@@ -0,0 +1,37 @@
+﻿namespace COM {
+    public class ClsTimer {
+        #region Fields & Properties
+
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; } = DateTime.MinValue;
+
+        #endregion
+        #region Constructors
+
+        public ClsTimer() {
+            // Empty
+        }
+
+        #endregion
+        #region Methods
+
+        public void Start() {
+			this.StartTime = DateTime.Now;
+            this.EndTime = DateTime.MinValue;
+		}
+
+        public void Stop() {
+            this.EndTime = DateTime.Now;
+        }
+
+        public TimeSpan GetElapsedTime() {
+			if (this.EndTime == DateTime.MinValue) {
+				return DateTime.Now - this.StartTime;
+			} else {
+				return this.EndTime - this.StartTime;
+			}
+		}
+
+        #endregion
+    }
+}
