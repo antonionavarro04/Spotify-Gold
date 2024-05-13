00e2716875b5622043bddd507ba2aa891398b22c
antonionavarro04 <antonio.navarro@iesnervion.es>
2024-05-13 23:16:15 +0200

    Files que me olvide

---
+++ ENT/Dto/Result/DtoResultResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT.Dto.Result {
+    public class DtoResultResponse {
+        #region Fields and Properties
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string Thumbnail { get; set; }
+
+        #endregion
+    }
+}
